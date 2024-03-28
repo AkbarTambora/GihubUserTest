2504302e1a33aa175bc063c39f73003d22889af0
RakaiTambora <akbar.khaerullah.9@gmail.com>
2024-03-28 15:40:12 +0700

    optimize imports

--- app/src/main/java/com/example/gihubusertest/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/gihubusertest/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.gihubusertest.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
